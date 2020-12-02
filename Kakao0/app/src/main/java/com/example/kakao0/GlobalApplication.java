173cc25af036eae564abf3f294c69933be8bc303
jhs1142002 <69894239+jhs1142002@users.noreply.github.com>
2020-12-02 21:01:22 +0900

    Create GlobalApplication.java

---
+++ Kakao0/app/src/main/java/com/example/kakao0/GlobalApplication.java
@@ -0,0 +1,75 @@
+package com.example.kakao0;
+
+import android.app.Activity;
+import android.app.Application;
+import android.graphics.Bitmap;
+
+import androidx.collection.LruCache;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+import com.kakao.auth.KakaoSDK;
+
+public class GlobalApplication extends Application {
+    private static volatile GlobalApplication instance = null;
+    private static volatile Activity currentActivity = null;
+    private ImageLoader imageLoader;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+
+        KakaoSDK.init(new KakaoSDKAdapter());
+
+        final RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        ImageLoader.ImageCache imageCache = new ImageLoader.ImageCache() {
+            final LruCache<String, Bitmap> imageCache = new LruCache<>(30);
+
+            @Override
+            public void putBitmap(String key, Bitmap value) {
+                imageCache.put(key, value);
+            }
+
+            @Override
+            public Bitmap getBitmap(String key) {
+                return imageCache.get(key);
+            }
+        };
+
+        imageLoader = new ImageLoader(requestQueue, imageCache);
+    }
+    public static Activity getCurrentActivity() {
+        return currentActivity;
+    }
+
+    public static void setCurrentActivity(Activity currentActivity) {
+        GlobalApplication.currentActivity = currentActivity;
+    }
+
+    /**
+     * singleton 애플리케이션 객체를 얻는다.
+     * @return singleton 애플리케이션 객체
+     */
+    public static GlobalApplication getGlobalApplicationContext() {
+        if(instance == null)
+            throw new IllegalStateException("this application does not inherit com.kakao.GlobalApplication");
+        return instance;
+    }
+
+    public ImageLoader getImageLoader() {
+        return imageLoader;
+    }
+
+    /**
+     * 애플리케이션 종료시 singleton 어플리케이션 객체 초기화한다.
+     */
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        instance = null;
+    }
+
+}
